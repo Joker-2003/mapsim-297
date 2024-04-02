69b593534af6affaf11a8bfb2bbe0e0d9093b0a8
Joker <srivartkovid@gmail.com>
2024-04-02 18:24:38 -0400

    fix: removed tutorials

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -44,11 +44,11 @@ export const Navigation = (props) => {
                 Services
               </a>
             </li> */}
-            <li>
+            {/* <li>
               <a href="#portfolio" className="page-scroll">
                 Tutorials
               </a>
-            </li>
+            </li> */}
             {/* <li>
               <a href="#testimonials" className="page-scroll">
                 Testimonials
